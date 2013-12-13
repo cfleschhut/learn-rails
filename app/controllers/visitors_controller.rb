259e6a177d05b6e4ddb09e18d1a8b453b47c1920
Christian Fleschhut <christianfleschhut@gmail.com>
2013-12-13 15:09:55 +0100

    example debugging code

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,10 @@
 class VisitorsController < ApplicationController
+
   def new
+    # logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
+    # logger.debug 'DEBUG: Owner name is ' + @owner.name
+    # raise 'Deliberate Failure'
   end
+
 end
