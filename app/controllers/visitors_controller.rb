29fa9d53879f82789ac49bccfb35411ca6b12b6c
Christian Fleschhut <weltrekordhalter@gmail.com>
2013-12-17 17:14:48 +0100

    contact form (old way)

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,12 +1,7 @@
 class VisitorsController < ApplicationController
 
   def new
-    # logger.debug 'DEBUG: entering new method'
     @owner = Owner.new
-    # logger.debug 'DEBUG: Owner name is ' + @owner.name
-    # raise 'Deliberate Failure'
-    flash.now[:notice] = 'flash notice'
-    flash.now[:alert] = 'flash alert'
   end
 
 end
